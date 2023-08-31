c10ccfa0bd9c234ffe39e851075622129586e747
BLaurent08 <belaurent08@gmail.com>
2023-08-31 10:20:56 -0400

    fixed TiDB

--- db-connection/db-connect.js
+++ db-connection/db-connect.js
@@ -5,7 +5,12 @@ const connection = mysql.createConnection({
     host: process.env.HOST,
     user: process.env.USER,
     password: process.env.PASSWORD,
-    database: process.env.DATABASE
+    database: process.env.DATABASE,
+    port: process.env.PORT,
+    ssl: {
+        minVersion: 'TLSv1.2',
+        rejectUnauthorized: true
+    }
 });
 
 module.exports = connection;
