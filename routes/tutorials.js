19154962e90d0d31642b7f97bbc223a45dc39c38
BLaurent08 <belaurent08@gmail.com>
2023-09-01 07:24:01 -0400

    added a "/tutorials"

--- routes/tutorials.js
+++ routes/tutorials.js
@@ -10,6 +10,13 @@ router.get('/', function(req, res, next) {
   });
 });
 
+router.get('/tutorials', function(req, res, next) {
+  connection.query('SELECT * FROM tutorials', function (error, results, fields) {
+    if (error) throw error;
+    res.json(results);
+  });
+});
+
 router.get('/:id', function(req, res, next) {
   connection.query('SELECT * FROM tutorials WHERE tutorialid = ?', [req.params.id], function (error, results, fields) {
     if (error) throw error;
